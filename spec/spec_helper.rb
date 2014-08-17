286558ca7f6941b62ad019a8a966143b7d80e6ae
Dylan Griffith <dyl.griffith@gmail.com>
2014-08-17 17:15:29 +1000

    only load app in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,6 @@
 RACK_ENV = 'test' unless defined?(RACK_ENV)
-require File.expand_path(File.dirname(__FILE__) + "/../main")
+require File.expand_path(File.dirname(__FILE__) + "/../app")
 require 'pry'
 
 RSpec.configure do |conf|
-end
-
-# You can use this method to custom specify a Rack app
-# you want rack-test to invoke:
-#
-#   app Tokens::App
-#   app Tokens::App.tap { |a| }
-#   app(Tokens::App) do
-#     set :foo, :bar
-#   end
-#
-def app(app = nil, &blk)
-  @app ||= block_given? ? app.instance_eval(&blk) : app
-  @app ||= Padrino.application
 end

5d9f58743d79f3534cbe0f12017e687eaeb6b865
Dylan Griffith <dyl.griffith@gmail.com>
2014-08-17 19:29:47 +1000

    always require string_utils due to poor loading strategy

--- app.rb
+++ app.rb
@@ -4,5 +4,6 @@ require 'coffee_script'
 require './downloads'
 require './assets'
 
+require './lib/string_utils'
 Dir["lib/**/*.rb"].each { |f| load(f) }
 Dir["models/**/*.rb"].each { |f| load(f) }
--- assets.rb
+++ assets.rb
@@ -1,5 +1,4 @@
 class DownloadOrganizer < Sinatra::Base
-
   get "/assets/css/*.css" do |css_file_name|
     sass :"sass/#{css_file_name}"
   end
